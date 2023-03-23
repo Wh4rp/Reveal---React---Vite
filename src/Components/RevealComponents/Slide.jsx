dd4536e25b061b6d8c9a96c45117b0f03e849641
Wh4rp <matias.fernandez@uc.cl>
2023-03-23 20:26:06 -0300

    added more sample slides

--- src/Components/RevealComponents/Slide.jsx
+++ src/Components/RevealComponents/Slide.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Slide = ({ children, transition = 'fade' }) => (
+const Slide = ({ children, transition = 'slide' }) => (
   <section data-transition={transition}>{children}</section>
 );
 
--- src/Slides/index.jsx
+++ src/Slides/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './Title';
+import AnotherSlide from './AnotherSlide';
 
 export default function Slides() {
   return (
     <>
       <Title />
+      <AnotherSlide />
     </>
   );
 }
